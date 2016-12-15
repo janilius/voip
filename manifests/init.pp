e5c457fea2889f4db810e2143eeea953f072c0ff
janilius <jani.markkula@myy.haaga-helia.fi>
2016-12-15 10:08:32 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,17 +2,17 @@ class voip {
 	package {mumble:
 		ensure => "installed",
 		allowcdrom => "true",
-		}
+	}
 	package {mumble-server:
 		ensure => "installed",
 		allowcdrom => "true",
-		}
+	}
 	file { '/etc/mumble-server.ini':
 		ensure => file,
 		owner => 'root',
 		group => 'root',
 		mode => '0644',
 		source => '/etc/puppet/modules/voip/mumble-server.ini',
-		}
+	}
 
 }
